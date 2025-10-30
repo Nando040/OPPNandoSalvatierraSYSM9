b02a51e2217d7f543d25e172781a11e27a8de95c
Nando040 <nando.salvatierra@outlook.com>
2025-10-30 16:47:07 +0100

    Skapade mina 2 första listor användare och recept, med test användare och recept

---
+++ Model/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPPNandoSalvatierraSYSM9.Model
+{
+     public class Recipe
+    {
+        public string Titel { get; set; } = "";
+
+        public string Instruktioner { get; set; } = ""; // lägger till (" ") för att undvika null värde, så det inte kraschar
+
+
+    }
+}
