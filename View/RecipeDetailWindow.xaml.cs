0fa214335e4a0d657ad4530bee8918983ddcb3c8
Nando040 <nando.salvatierra@outlook.com>
2025-11-06 14:28:17 +0100

    Kommenterade vad Converter gör och Viewmodel

--- View/RecipeDetailWindow.xaml.cs
+++ View/RecipeDetailWindow.xaml.cs
@@ -33,21 +33,26 @@ namespace OPPNandoSalvatierraSYSM9.View
             DataContext = vm; // Kopplar upp denna filen till xaml och ViewModelen dvs till all {Binded} i xaml
         }
 
-        private sealed class NotConverter : IValueConverter
+        private sealed class NotConverter : IValueConverter // är en interface som finns i systems. dvs. .NET biblioteket
+        // som kan hämtas av alla som behöver den. I detta fall används den för att ändra bool värden i boxen som vi vill redigera
+        //så det är möjligt att sätta i detta fall nya Recept
         {
             public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is bool b) return !b;
-            if (value is bool?) return value is bool nb && nb ? false : true;
-            return DependencyProperty.UnsetValue;
-        }
+            {
+                if (value is bool b) return !b; // om värdet är true/False så returneras motsatsen
+                if (value is bool?) return value is bool nb && nb ? false : true;//dvs att true blir false och false blir true
+                //= det går att redigera/detta är ReadOnly
+                return DependencyProperty.UnsetValue;// om det inte är en bool så kan den inte läsa av
+                // och den meddelar att den inte kan hantera det så det är bättre att ignorera det
 
-            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is bool b) return !b;
-            if (value is bool?) return value is bool nb && nb ? false : true;
-            return DependencyProperty.UnsetValue;
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) // denna metod gör samma sak som Convert
+             // fast denna skickar data tiilbaka till VM så resten av appen vet vad som händer
+            {
+                if (value is bool b) return !b;
+                if (value is bool?) return value is bool nb && nb ? false : true;
+                return DependencyProperty.UnsetValue;
+            }
         }
     }
-    }
+    
 }
